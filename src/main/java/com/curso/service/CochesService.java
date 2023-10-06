6501eee14078e06bdf72860508b9e744fa8cb5b1
luismiguelviewnext <135017297+luismiguelviewnext@users.noreply.github.com>
2023-10-06 11:00:54 +0200

    cambios

--- src/main/java/com/curso/service/CochesService.java
+++ src/main/java/com/curso/service/CochesService.java
@@ -6,6 +6,7 @@ import com.curso.model.Coches;
 
 /**
  * Interfaz que define las operaciones CRUD para la entidad Coches.
+ * 
  * @author Luis Miguel Albarracín Castillo
  */
 public interface CochesService {
@@ -46,8 +47,19 @@ public interface CochesService {
      */
     void eliminar(int id);
 
-
+    /**
+     * Obtiene una lista de coches basados en la marca especificada.
+     * 
+     * @param marca La marca de coche que se desea buscar.
+     * @return Una lista de coches que coinciden con la marca dada.
+     */
     List<Coches> encuentraCochePorMarca(String marca);
 
+    /**
+     * Obtiene una lista de coches basados en el modelo especificado.
+     * 
+     * @param modelo El modelo de coche que se desea buscar.
+     * @return Una lista de coches que coinciden con el modelo dado.
+     */
     List<Coches> encuentraCochePorModelo(String modelo);
 }
